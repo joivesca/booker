f1b41bbb63ab3024db45cb3fd64ad4b13e1da5b6
zerodev <joivesca@gmail.com>
2025-03-05 06:14:51 +0000

    Add files via upload

---
+++ src/main/java/com/onndoo/booker/model/Book.java
@@ -0,0 +1,35 @@
+package com.onndoo.booker.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Books")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String author;
+	
+	
+}
