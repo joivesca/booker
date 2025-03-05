880c0da55c9ab81ed7fbb61765650dad0279e6f4
Joseph Espinosa <joivesca@gmail.com>
2025-03-05 03:19:07 -0400

    Changes to clean code

--- src/main/java/com/onndoo/booker/model/Book.java
+++ src/main/java/com/onndoo/booker/model/Book.java
@@ -21,6 +21,8 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Book implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
